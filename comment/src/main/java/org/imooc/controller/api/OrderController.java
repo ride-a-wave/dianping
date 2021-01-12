6dd4bf587260c84ae99a8055e6b8c4b8eb1d245d
ride-a-wave <15720606503@163.com>
2021-01-12 23:53:31 +0800

    team2第一天工作 订单

---
+++ comment/src/main/java/org/imooc/controller/api/OrderController.java
@@ -0,0 +1,10 @@
+package org.imooc.controller.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+    public void addOrder(){
+        System.out.println("team2 第一天 订单");
+    }
+}
