cb482ebee2ad2923a7d5966ac6831e262ac7ef74
ride-a-wave <15720606503@163.com>
2021-01-12 23:51:51 +0800

    team1第二天工作 更新购物车功能

--- comment/src/main/java/org/imooc/controller/api/CartController.java
+++ comment/src/main/java/org/imooc/controller/api/CartController.java
@@ -8,4 +8,9 @@ public class CartController {
     public void addCart(){
         System.out.println("team1 开发购物车 第一天");
     }
+
+    public void updateCart(){
+        System.out.println("team1 开发购物车 第二天");
+    }
+
 }
